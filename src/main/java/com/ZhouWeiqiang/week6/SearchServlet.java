6967b1f77c5f8b248b46da5be0d2c9ce220ca1fc
2019211001000713ZhouWeiqiang <1179745132@qq.com>
2021-04-11 21:15:11 +0800

    6.HE Week6

--- src/main/java/com/ZhouWeiqiang/week6/SearchServlet.java
+++ src/main/java/com/ZhouWeiqiang/week6/SearchServlet.java
@@ -4,23 +4,23 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.net.URLEncoder;
 
 @WebServlet(name = "SearchServlet", urlPatterns = {"/search"})
 public class SearchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String txt=new String(request.getParameter("txt").getBytes("iso-8859-1"),"gbk");
-        //The solution to Chinese garbled code found on the Internet does not work
+        String txt=request.getParameter("txt");
         String search=request.getParameter("search");
         if("".equals(txt.trim()))
             response.sendRedirect("index.jsp");
         else{
             if(search.equals("baidu")){
-                response.sendRedirect("https://www.baidu.com/s?wd="+txt);
+                response.sendRedirect("https://www.baidu.com/s?wd="+ URLEncoder.encode(txt));
             }else if(search.equals("bing")){
-                response.sendRedirect("https://cn.bing.com/search?q="+txt);
+                response.sendRedirect("https://cn.bing.com/search?q="+URLEncoder.encode(txt));
             }else if(search.equals("google")){
-                response.sendRedirect("https://www.google.com/search?q="+txt);
+                response.sendRedirect("https://www.google.com/search?q="+URLEncoder.encode(txt));
             }
         }
     }
